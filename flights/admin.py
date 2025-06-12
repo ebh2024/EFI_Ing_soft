5b0540e720553f199eb7d0bfa57171b14a77b577
ebh2024 <e.bustohurtado@itecriocuarto.org.ar>
2025-06-12 20:48:59 -0300

    Added aircraft model and flight duration

--- flights/admin.py
+++ flights/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Flight, Passenger, Booking
+from .models import Flight, Passenger, Booking, Aircraft
 
 admin.site.register(Flight)
 admin.site.register(Passenger)
 admin.site.register(Booking)
+admin.site.register(Aircraft)
--- flights/models.py
+++ flights/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+class Aircraft(models.Model):
+    name = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
 class Flight(models.Model):
     flight_number = models.CharField(max_length=20)
     origin = models.CharField(max_length=50)
     destination = models.CharField(max_length=50)
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
+    duration = models.DurationField(null=True, blank=True)
+    aircraft = models.ForeignKey(Aircraft, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.flight_number} - {self.origin} to {self.destination}"
