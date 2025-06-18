cecb8dc4e1d7a5db8c9e252618114f711a5107d6
ebh2024 <e.bustohurtado@itecriocuarto.org.ar>
2025-06-18 16:06:09 -0300

    Fixed NameError in admin.py (again)

--- flights/admin.py
+++ flights/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Flight, Passenger, Booking, Aircraft, Seat, Ticket
+from .models import Flight, Passenger, Booking, Aircraft, Seat, Ticket, Usuario
 
 admin.site.register(Flight)
 admin.site.register(Passenger)
